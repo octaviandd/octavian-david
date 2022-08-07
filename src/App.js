dbb296fda4a9a75221241714a2f0b750ecf2febc
Octavian David <45077244+octaviandd@users.noreply.github.com>
2022-08-07 14:02:56 +0100

    [master] - Remove firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
+/** @format */
+
 import React from "react";
 import ContactForm from "./ContactForm.js";
 import Header from "./Header.js";
 import Timeline from "./Timeline";
 import Tools from "./Tools.js";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB7Czluv2wdnhkjw3p2btADERm_-ZAsUng",
-  authDomain: "personal-project-e1202.firebaseapp.com",
-  projectId: "personal-project-e1202",
-  storageBucket: "personal-project-e1202.appspot.com",
-  messagingSenderId: "1035152969756",
-  appId: "1:1035152969756:web:cb8fbb1c269639bdef8fd9",
-  measurementId: "G-93LPZLCRT4",
-};
-
-const app = initializeApp(firebaseConfig);
-
-const analytics = getAnalytics(app);
 
 const App = () => {
   return (
